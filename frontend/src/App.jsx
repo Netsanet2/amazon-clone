52774d954c4c22a9e18d112cec71fc9a417e51ac
samrita <samrawitasmelash41@gmail.com>
2026-09-06 07:50:22 -0700

    Implement cart functionality (#4)
    Co-authored-by: Samrawit Asmelash <samrawitasmelashh41@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Cart from "./pages/cart/Cart";
 function App() {
   return (
     <BrowserRouter>
@@ -8,7 +8,7 @@ function App() {
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/products" element={<h1>Products Page</h1>} />
         <Route path="/product/:id" element={<h1>Product Details</h1>} />
-        <Route path="/cart" element={<h1>Cart Page</h1>} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<h1>Checkout Page</h1>} />
         <Route path="/orders" element={<h1>Orders Page</h1>} />
         <Route path="/login" element={<h1>Login Page</h1>} />
